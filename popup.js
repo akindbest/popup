996f67f93c70cba16fb6d671f0cbec19deab587b
akindbest <akindbest@gmail.com>
2022-08-11 11:44:56 +0100

    update project

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 var btn = document.getElementById("btn");
 btn.onclick = () =>{
    const modal = document.getElementById("modal");
+   document.querySelector(".modal-content").classList.remove("animate-modal");
    let allDiv = document.querySelectorAll(".modal-content > div");
    allDiv.forEach(div => {
     div.style.display = "none";
    })
    modal.style.display = "flex";
    allDiv[0].style.display = "flex";
+   document.querySelector(".modal-content").classList.add("animate-modal");
+
 }
