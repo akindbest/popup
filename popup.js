564a95c6ae056896a437e12516da794a98d02122
akindbest <akindbest@gmail.com>
2022-08-11 12:34:55 +0100

    akin

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 var btn = document.getElementById("btn");
 btn.onclick = () =>{
+   showModal(0)
+}
+
+const showModal = (index)=>{
    const modal = document.getElementById("modal");
    document.querySelector(".modal-content").classList.remove("animate-modal");
    let allDiv = document.querySelectorAll(".modal-content > div");
@@ -7,7 +11,19 @@ btn.onclick = () =>{
     div.style.display = "none";
    })
    modal.style.display = "flex";
-   allDiv[0].style.display = "flex";
+   allDiv[index].style.display = "flex";
    document.querySelector(".modal-content").classList.add("animate-modal");
+}
 
+const closeModal = () =>{
+   document.getElementById("modal").style.display = "none";
+   document.querySelector('.modal-content').classList.remove('animate-modal');
+   let allDiv = document.querySelectorAll(".modal-content > div");
+   allDiv.forEach(div =>{
+      div.style.display = "none";
+   })
+
+const responseStatus = () =>{
+   
+}  
 }
